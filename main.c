01107296ecb0e620732abf8030d49747357fd600
DanielKubanek <danielkubanek@gmail.com>
2018-10-12 14:46:21 +0200

    zwiekszanie_zmniejszanie_1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+
+
+    int a = 4;
+    int b = 7;
+
+    printf("%i\n", a++ + b);
+    printf("%i\n", ++a +b);
+
+    printf("%i\n", --a + b);
+    printf("%i\n", a-- + b);
+
+
+
+    return 0;
+}
